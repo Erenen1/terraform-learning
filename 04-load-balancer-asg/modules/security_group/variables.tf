9f63e2cc5821bca62886305ea20967f2f754ddc7
Erenen1 <erencelikk212@gmail.com>
2025-01-29 18:56:43 +0300

    security group module added

---
+++ 04-load-balancer-asg/modules/security_group/variables.tf
@@ -0,0 +1,14 @@
+variable "security_group_name" {
+  description = "value"
+  type = string
+}
+variable "vpc_id" {
+  description = "value"
+  type = string
+}
+
+variable "allowed_ssh_ip_block" {
+  description = "value"
+  type = string
+}
+
