5dd0d2b551ce8075781559a67d0a0f11b454586f
Erenen1 <erencelikk212@gmail.com>
2025-01-29 15:15:16 +0300

    feat: VPC modülü için değişken tanımları eklendi

---
+++ 03-multi-environment/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR bloğu"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "Subnet CIDR bloğu"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "VPC ismi"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Subnet ismi"
+  type        = string
+}
